ae0a1bae768bb39763d26a7f47896f236d363e63
Joe Babbitt <jrebbabbitt@gmail.com>
2018-02-06 20:01:49 -0500

    Constants made final

--- src/org/usfirst/frc/team334/robot/Constants.java
+++ src/org/usfirst/frc/team334/robot/Constants.java
@@ -2,28 +2,28 @@ package org.usfirst.frc.team334.robot;
 
 public class Constants {
 	//Motors
-	public static int DRIVETRAIN_L_FRONT = 0;
-	public static int DRIVETRAIN_L_MIDDLE = 1;
-	public static int DRIVETRAIN_L_BACK = 2;
-	public static int DRIVETRAIN_R_FRONT = 3;
-	public static int DRIVETRAIN_R_MIDDLE = 4;
-	public static int DRIVETRAIN_R_BACK = 5;
+	public static final int DRIVETRAIN_L_FRONT = 0;
+	public static final int DRIVETRAIN_L_MIDDLE = 1;
+	public static final int DRIVETRAIN_L_BACK = 2;
+	public static final int DRIVETRAIN_R_FRONT = 3;
+	public static final int DRIVETRAIN_R_MIDDLE = 4;
+	public static final int DRIVETRAIN_R_BACK = 5;
 
 	//Pancake Solenoids
-	public static int PANCAKE_L_INPUT = 0;
-	public static int PANCAKE_L_OUTPUT = 1;
-	public static int PANCAKE_R_INPUT = 2;
-	public static int PANCAKE_R_OUTPUT = 3;
+	public static final int PANCAKE_L_INPUT = 0;
+	public static final int PANCAKE_L_OUTPUT = 1;
+	public static final int PANCAKE_R_INPUT = 2;
+	public static final int PANCAKE_R_OUTPUT = 3;
 
 	//Compressor
-	public static int COMPRESSOR = 0;
-	public static int PRESSURE_SENSOR = 1;
-	public static int FORCE_COMPRESSOR_ON_BTN = 2;
+	public static final int COMPRESSOR = 0;
+	public static final int PRESSURE_SENSOR = 1;
+	public static final int FORCE_COMPRESSOR_ON_BTN = 2;
 
 	//Elevator
-	public static int ELEVATOR_MOTOR_1 = 6;
-	public static int ELEVATOR_MOTOR_2 = 7;
+	public static final int ELEVATOR_MOTOR_1 = 6;
+	public static final int ELEVATOR_MOTOR_2 = 7;
 
 	// Vision
-	public static String VISION_NT_NAME = "vision";
+	public static final String VISION_NT_NAME = "vision";
 }
