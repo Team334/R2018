07635cd1d90a95c4eebb32d7878dd0f8648f2bf2
apanas9246 <apanas9246@bths.edu>
2018-02-20 13:03:06 -0500

    Add Intake & Climber

---
+++ src/org/usfirst/frc/team334/robot/commands/climber/ExtendClimberCommand.java
@@ -0,0 +1,40 @@
+package org.usfirst.frc.team334.robot.commands.climber;
+
+import org.usfirst.frc.team334.robot.Constants;
+import org.usfirst.frc.team334.robot.Robot;
+import edu.wpi.first.wpilibj.command.Command;
+
+public class ExtendClimberCommand extends Command {
+
+    public ExtendClimberCommand() {
+        requires(Robot.sClimber);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	System.out.println("EXTENDCLIMBERCOMMAND HAS BEEN INITIALIZED");
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.sClimber.setClimberSpeed(Constants.CLIMBER_EXTEND_SPEED);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return Robot.sClimber.getTopLimitSwitch();
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.sClimber.setClimberSpeed(0);
+    	System.out.println("EXTENDCLIMBERCOMMAND HAS ENDED");
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	Robot.sClimber.setClimberSpeed(0);
+    	System.out.println("EXTENDCLIMBERCOMMAND HAS BEEN INTERRUPTED");
+    }
+}
